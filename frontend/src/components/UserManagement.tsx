f08541269d84122bc396cf13b4d93722f78abe40
lauripalonen <palonen.lauri@gmail.com>
2021-12-10 13:38:37 +0200

    hide settings page from non-admins, add disable user mngmt view from non-admins

--- frontend/src/components/UserManagement.tsx
+++ frontend/src/components/UserManagement.tsx
@@ -3,9 +3,22 @@ import { useQuery } from '@apollo/client'
 import { ALL_USERS } from '../graphql/queries'
 import Container from '@material-ui/core/Container'
 import UserTable from './UserTable'
+import { UserType } from '../types'
 
-const UserManagement = () => {
-  const { data , loading, error } = useQuery(ALL_USERS)
+interface Props {
+  user: UserType | undefined
+}
+
+const UserManagement = ({ user }: Props) => {
+  const { data, loading, error } = useQuery(ALL_USERS)
+
+  if (user?.user_role !== 'admin') {
+    return (
+      <h1>
+        Not Authorized
+      </h1>
+    )
+  }
 
   if (loading) {
     return <p>Loading...</p>
@@ -14,10 +27,10 @@ const UserManagement = () => {
   if (error) {
     return <p>Error occurred while fetching users</p>
   }
-  
+
   return (
     <Container>
-      <UserTable users={data.getAllUsers}/>
+      <UserTable users={data.getAllUsers} />
     </Container>
   )
 }
